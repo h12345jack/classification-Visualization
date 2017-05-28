3ef2622dad33bf8a4a617d61021ab94fa89188f2
h12345jack <h12345jack@bytedance.com>
2017-05-29 00:40:55 +0800

    api modify

--- src/api/tree.js
+++ src/api/tree.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const ONLINE_URL = 'https://h12345jack.github.io/classification-Visualization/v1'
+const ONLINE_URL = 'https://h12345jack.github.io/classification-Visualization/v1/data'
 const DEV_URL = '/data'
 const DEBUG = false
 const API_URL = DEBUG? DEV_URL : ONLINE_URL
